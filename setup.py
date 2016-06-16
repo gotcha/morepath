9c3f7ca12861fed95e53406b11c3ab7ce44e40e1
Henri Hulski <henri.hulski@gazeta.pl>
2016-06-16 19:53:25 +0200

    Update setup.py and CHANGES.txt

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Development Status :: 5 - Production/Stable'
     ],
     keywords="web wsgi routing morepath",
